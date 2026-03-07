89cf4bed2f2570a3b5ac0cb172c2c400e282e447
Jitesh7140 <jiteshsaini2005@gmail.com>
2026-03-07 09:58:34 +0530

    07-03-2026

--- instagram_clone/schema.sql
+++ instagram_clone/schema.sql
@@ -40,6 +40,32 @@ primary key (user_id, photo_id)
 insert into likes(user_id, photo_id) values (1, 1);
 insert into likes(user_id, photo_id) values (1, 2);
 
+-- 5. create a table for followers with the following columns:
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+-- 6. create a table for tags with the following columns:
+CREATE TABLE tags (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    tag_name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- 7. create a table for photo_tags with the following columns:
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
 
  select * from comments;
  select * from photos;
